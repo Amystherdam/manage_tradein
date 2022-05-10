648358b857b7a31b09c63ab79ed25499fd39ccec
Amystherdam <pro.lucas07@gmail.com>
2022-05-10 12:29:59 -0300

    Product CRUD creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_144602) do
+ActiveRecord::Schema.define(version: 2022_05_10_152122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2022_05_10_144602) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.date "birthdate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
