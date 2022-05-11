c7ebee8d53acbd146ee2ea201e40a8c3679a3fac
Amystherdam <pro.lucas07@gmail.com>
2022-05-11 14:02:20 -0300

    Bug fix related to missing image registered

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def set_img(object)
+    default_img = '/assets/avatar-default.png'
+
+    case object.class.to_s
+    when 'Customer'
+      object.avatar.attached? ? object.avatar : default_img
+    when 'Product'
+      object.packaging.attached? ? object.packaging : default_img
+    when 'Sale'
+      outgoing_invoice = object.outgoing_invoice
+      outgoing_invoice.attached? ? outgoing_invoice : default_img
+    when 'User'
+      picture = object.user_profile.picture
+      picture.attached? ? picture : default_img
+    else
+      default_img
+    end
+  end
 end
