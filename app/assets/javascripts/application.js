4d23fc97eb4612995bfdb799a24c28847ef2de1d
Amystherdam <pro.lucas07@gmail.com>
2022-05-11 23:50:42 -0300

    Initial implementation of a frontend template in the application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,16 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
 //= require activestorage
-//= require_tree .
+
+//= require jquery.min.js
+//= require popper.min.js
+//= require bootstrap.min.js
+//= require jquery.scrollbar.min.js
+//= require jquery-scrollLock.min.js
+//= require moment.min.js
+//= require fullcalendar.min.js
+//= require autosize.min.js
+//= require sweetalert2.min.js
+
+//= require app.min.js
+
+//= require more_calendar.js
