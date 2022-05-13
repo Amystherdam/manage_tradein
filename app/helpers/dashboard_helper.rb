e41b5e6f1cdb31395e71bac94fae7d64772a430c
Amystherdam <pro.lucas07@gmail.com>
2022-05-13 09:50:31 -0300

    Creating buyer ranking

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -9,12 +9,16 @@ module DashboardHelper
     target_att_value[0].to_i
   end
 
-  def total_sales_amount(sales_by_month)
+  def total_sales_amount(sales_by_month, sales_product_data = nil)
     product_ids = []
     product_quantities = []
     total_sale = 0.0
 
-    sales_product_data = sales_by_month.reject {|object| object.class.to_s != "Array"}.flatten.map(&:product).flatten
+    if sales_product_data
+      sales_product_data = sales_product_data.flatten
+    else
+      sales_product_data = sales_by_month.reject {|object| object.class.to_s != "Array"}.flatten.map(&:product).flatten
+    end
 
     sales_product_data.each do |sale_product_data|
       product_ids.push extract_attrs_sale_data(/id=>\d{1,}/, sale_product_data)
@@ -27,5 +31,27 @@ module DashboardHelper
     end
 
     total_sale
+  end
+
+  def top_customers(sales_by_months)
+    total_per_customers = []
+    rank_month_customers = []
+    month = 1
+
+    sales_by_months.each do |sales_by_month|
+      sales_by_month.reject {|object| object.class.to_s != "Array"}.flatten.group_by {|sale| sale.customer }.each do |customer, sales|
+        sales_product_data = sales.map(&:product)
+        total_per_customers.push [customer.name, total_sales_amount(sales, sales_product_data)]
+      end
+
+      total_per_customers.sort_by {|customer, total| total }.reverse.take(3).each do |total_per_customer| 
+        rank_month_customers.push [month, "#{total_per_customer[0]}", total_per_customer[1]]
+      end
+
+      total_per_customers = []
+      month += 1
+    end
+
+    rank_month_customers
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ApplicationRecord
+  has_many :sales
   has_one_attached :avatar
 end
